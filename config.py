67f532a26bd97866a9368e99d4c371bf806f6aa7
GuotaoWang <42886966+guotaowang@users.noreply.github.com>
2020-03-18 15:59:22 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+# sbu_training_root = './Datasets/SBU/SBUTrain4KRecoveredSmall'
+# sbu_training_root = 'F:\\BDRAR-master\\Datasets\\SBU\\MSRA10K_Imgs_GT\\DAVISFDPALLIMAGE\\'
+# sbu_training_root = 'F:\\Train_video\\Train_Data_FDP\\aa\\'
+sbu_training_root = 'F:\\Train_video\\Train_Data_FDP\\Trian_Data_RGB\\'
+# sbu_training_root = 'F:\\Train_video\\Train_FDP_Ima_rand\\'
+sbu_testing_root = 'F:\\DAVSOD-master\\Datasets\\MINE\\FBMS'
+resnext_101_32_path = 'resnext_101_32x4d.pth'
